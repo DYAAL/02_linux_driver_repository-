70a326694d5b254c9ee8145b1c007daf8a6af312
youyoung wang <youyoungwang@gmai.com>
2024-06-08 00:24:59 +0800

    add

--- src/driver_func_drv.c
+++ src/driver_func_drv.c
@@ -1,17 +1,22 @@
-#include "asm-generic/errno.h"
+
 #include "drv_define.h"
+#include "linux/err.h"
 #include "linux/gfp.h"
+#include "linux/kdev_t.h"
 #include "linux/kern_levels.h"
+#include "linux/printk.h"
+#include <asm/io.h>
+#include <linux/device.h>
 #include <linux/fs.h>
 #include <linux/slab.h>
 
-ssize_t device_write(struct file*, const char __user*, size_t, loff_t*)
+ssize_t device_write(struct file* file_t, const char __user* u_buffer, size_t size, loff_t* offset)
 {
     // TODO fix this!!
     return 0;
 }
 
-ssize_t device_read(struct file*, char __user*, size_t, loff_t*)
+ssize_t device_read(struct file* file_t, char __user* u_buffer, size_t size, loff_t* offset)
 {
     // TODO fix this!!
     return 0;
@@ -23,14 +28,44 @@ static const struct file_operations device_fops = {
     .write = device_write,
     // ... 其他操作函数 ...
 };
-void resource_recycle(Hello_Device_T this, int recycle_type)
+void resource_recycle(Hello_Device_T this, Enum_Name recycle_type)
 {
-    
-    // TODO fix this!!
+    switch (recycle_type) {
+    case KMALLOC_DEVICE_ERROR:
+        break;
+
+    case CHR_DEV_REGISTER_ERROR:
+        kfree(this);
+        break;
+    case CLASS_REGISTER_ERROR:
+        unregister_chrdev(this->dev_major, this->chr_dev_name);
+        kfree(this);
+        break;
+    case DEVICE_REGISTER_ERROR:
+        class_destroy(this->class_dev.cls);
+        unregister_chrdev(this->dev_major, this->chr_dev_name);
+        kfree(this);
+        break;
+    case IOREMAP_ERROR:
+        device_destroy(this->class_dev.cls, MKDEV(this->dev_major, this->dev_minor));
+        class_destroy(this->class_dev.cls);
+        unregister_chrdev(this->dev_major, this->chr_dev_name);
+        kfree(this);
+
+        break;
+    default:
+        break;
+    }
 }
 int chr_dev_register(Hello_Device_T this)
 {
-    // TODO fix this!!
+    this->dev_major = register_chrdev(this->dev_major, this->chr_dev_name, this->fops);
+    if (this->dev_major < 0) {
+        printk(KERN_ERR "register_chrdev failed with %d\n", this->dev_major);
+        resource_recycle(this, CHR_DEV_REGISTER_ERROR);
+        return -ENODEV;
+    }
+    return 0;
 }
 
 int device_init(Hello_Device_T this, int flags)
@@ -39,6 +74,7 @@ int device_init(Hello_Device_T this, int flags)
     this = kmalloc(sizeof(*this), GFP_KERNEL);
     if (this == NULL) {
         printk(KERN_ERR "Failed to allocate memory for device\n");
+        resource_recycle(this, KMALLOC_DEVICE_ERROR);
         return -ENOMEM;
     }
     this->dev_major = 0;
@@ -48,5 +84,33 @@ int device_init(Hello_Device_T this, int flags)
     this->functions_pointer.resource_recycle = resource_recycle;
     this->functions_pointer.chr_dev_register = chr_dev_register;
 
+    return 0;
+}
+
+int class_dev_register(Hello_Device_T this)
+{
+    this->class_dev.cls = class_create(THIS_MODULE, this->class_name);
+    if (IS_ERR(this->class_dev.cls)) {
+        printk(KERN_ERR "class_create failed with %ld\n", PTR_ERR(this->class_dev.cls));
+        resource_recycle(this, CLASS_REGISTER_ERROR);
+        return PTR_ERR(this->class_dev.cls);
+    }
+    this->class_dev.dev = device_create(this->class_dev.cls, NULL, MKDEV(this->dev_major, this->dev_minor), NULL, this->chr_dev_name);
+    if (IS_ERR(this->class_dev.dev)) {
+        printk(KERN_ERR "device_create failed with %ld\n", PTR_ERR(this->class_dev.dev));
+        resource_recycle(this, DEVICE_REGISTER_ERROR);
+        return PTR_ERR(this->class_dev.dev);
+    }
+    return 0;
+}
+
+int device_ioremap(Hello_Device_T this, unsigned long phys_addr, unsigned long size)
+{
+    this->virt_registers.virt_register_base = ioremap(this->registers.register_base, sizeof(this->registers.register_base));
+    if (IS_ERR(this->virt_registers.virt_register_base)) {
+        printk(KERN_ERR "ioremap failed with %ld\n", PTR_ERR(this->virt_registers.virt_register_base));
+        resource_recycle(this, IOREMAP_ERROR);
+        return PTR_ERR(this->virt_registers.virt_register_base);
+    }
     return 0;
 }
--- src/drv_define.h
+++ src/drv_define.h
@@ -1,14 +1,15 @@
 #ifndef __DRV_DEFINE_H__
 #define __DRV_DEFINE_H__
 
-#include "linux/types.h"
 #include <linux/fs.h>
 
 typedef enum {
     SUCCESS = 0,
-    KMALLOC_DEVICE_ERROR = -1,
-    CLASS_REGISTER_ERROR = -2,
-    DEVICE_REGISTER_ERROR = -3,
+    KMALLOC_DEVICE_ERROR,
+    CHR_DEV_REGISTER_ERROR,
+    CLASS_REGISTER_ERROR,
+    DEVICE_REGISTER_ERROR,
+    IOREMAP_ERROR,
 
 } Enum_Name;
 
@@ -19,9 +20,14 @@ typedef struct {
 } Class_Device, *Class_Device_T;
 
 typedef struct {
-    void* register_base;
+    unsigned int register_base;
 
 } Registers, *Registers_T;
+
+typedef struct {
+    void* virt_register_base;
+
+} Virt_Registers, *Virt_Registers_T;
 
 // 前向声明Hello_Device_T，以便在Functions_Pointer中使用
 typedef struct Hello_Device* Hello_Device_T;
@@ -29,22 +35,30 @@ typedef struct Hello_Device* Hello_Device_T;
 // 定义Functions_Pointer结构体，使用前向声明的Hello_Device_T
 typedef struct {
     void (*resource_recycle)(Hello_Device_T this, Enum_Name recycle_type);
-    int (*chr_class_register)(Hello_Device_T this);
     int (*chr_dev_register)(Hello_Device_T this);
+    int (*class_dev_register)(Hello_Device_T this);
+    int (*device_ioremap)(Hello_Device_T this, unsigned long phys_addr, unsigned long size);
+    int (*device_iounmap)(Hello_Device_T this, void* virt_addr);
 } Functions_Pointer, *Functions_Pointer_T;
 
 // 现在可以完整定义Hello_Device结构体了
-typedef struct Hello_Device {
+struct Hello_Device {
     unsigned int dev_major;
     unsigned int dev_minor;
+    char* chr_dev_name;
+    char* class_name;
+    char* dev_name;
     // 假设Class_Device和Registers类型已经定义
     Class_Device class_dev;
     Registers registers;
+    Virt_Registers virt_registers;
     Functions_Pointer functions_pointer;
     const struct file_operations* fops;
-} Hello_Device, *Hello_Device_T; // 注意：这里的Hello_Device_T是重复的，但编译器通常允许
+}; // 注意：这里的Hello_Device_T是重复的，但编译器通常允许
 
 int device_init(Hello_Device_T this, int flags);
 void resource_recycle(Hello_Device_T this, Enum_Name recycle_type);
 int chr_dev_register(Hello_Device_T this);
+int class_dev_register(Hello_Device_T this);
+int device_ioremap(Hello_Device_T this, unsigned long phys_addr, unsigned long size);
 #endif // __DRV_DEFINE_H__
